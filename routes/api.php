96f7647d9c072798342744bf97caff0421b19e73
Bruna Vitória <brunavitoriati@gmail.com>
2024-07-28 18:04:18 -0300

    feat(Criando rotas e controladores): Cria as rotas da api de categorias e subcategorias

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\SubcategoriesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResources([
+        'categories' => CategoriesController::class,
+        'category/{category}/subcategories' => SubcategoriesController::class,
+    ]);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
