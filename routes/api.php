345abe3002503a679ce7d93e09b3de7bb324968d
Bruna Vitória <brunavitoriati@gmail.com>
2024-07-28 20:10:34 -0300

    feat(Criando rotas e controladores): Cria as rotas de autenticação registro, login e logout

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\SubcategoriesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::post('/register', 'register');
+});
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResources([
@@ -13,4 +19,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
